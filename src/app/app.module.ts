57e6e10a91b16cdf193b8e13519d88b7ef5e94ba
stevenglasford <stevenglasford@gmail.com>
2020-05-06 13:30:52 -0500

    Add the four main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MyListingsComponent } from './my-listings/my-listings.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    MyListingsComponent,
+    MessagesComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule

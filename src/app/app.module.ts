9baa2b392a33c7ecbf26394cc0e768a7d4c87e2c
stevenglasford <stevenglasford@gmail.com>
2020-05-06 16:20:46 -0500

    Add ability to add a new dog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
@@ -22,7 +23,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { DatabaseService } from '../database.service';
+import { Dog } from '../dog';
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +11,38 @@ import { DatabaseService } from '../database.service';
 
 export class ProfileComponent implements OnInit {
   user: User = {id: 3, username: 'waka', password: 'password', profilePhotoUrl: '', location: ''};
-  
+  usersDogs: Dog
+  dogName: string = '';
+  dogOwner: string = this.user.username;
+  dogPhoto: string = '';
+  dogLocation: string = '';
+  //debugging set to false in production
+  isSelected: boolean = false;
+
+
   constructor(private db:DatabaseService) { }
   ngOnInit(): void {
     this.user = this.db.getUserById(this.db.currentUser);
   }
   // username: string = this.user.username;
+  addDog():void {
+    this.db.addNewDog(this.dogName,this.user.id,this.dogPhoto,this.dogLocation);
+    // console.log(this.dogName);
+    // console.log(this.user.id);
+    // console.log(this.dogPhoto);
+    // console.log(this.dogLocation);
+    //reset the fields after the data is sent to the service
+    this.dogName='';
+    this.dogOwner='';
+    this.dogPhoto='';
+    this.dogLocation='';
+    this.isSelected=false;
 
+    
+  }
+
+  iWantToAdd(): void{
+    this.isSelected = true;
+  }
+  
 }

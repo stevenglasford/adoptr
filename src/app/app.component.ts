083a7be4d626d24bb0e6ce4f528ecbc10f0cc8eb
stevenglasford <stevenglasford@gmail.com>
2020-05-06 18:01:05 -0500

    Add very insecure login system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,34 @@ export class AppComponent {
   //0=home page
   //1=messages page
   //2=profile page
+  USERNAME: string = '';
+  PASSWORD: string = '';
+  isUserReal: number = -1;
+  selected: number = -1;
+
   constructor(private db:DatabaseService) { }
+
+  login():void{
+    //check if the user exists in the database
+    if(this.db.verifyUser(this.USERNAME,this.PASSWORD)){
+      this.selected=0;
+      this.isUserReal=-1;
+    }
+    else{
+      this.isUserReal=0;
+    }
+  }
+
+  logOut():void{
+    this.USERNAME='';
+    this.PASSWORD='';
+    this.selected=-1;
+  }
 
   ngOnInit() {
     this.db.currentUser = 2;
   }
 
-  selected: number = 0;
   home(): void{
     this.selected = 0;
   }
